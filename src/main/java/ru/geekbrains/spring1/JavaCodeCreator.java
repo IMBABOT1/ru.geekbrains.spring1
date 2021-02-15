ae1a06bdc3dc1e999d58f31bc8aeea3478a65d2c
IMBABOT1 <zlovred12@gmail.com>
2021-02-16 02:03:09 +0300

    createAnimalNames

--- src/main/java/ru/geekbrains/spring1/JavaCodeCreator.java
+++ src/main/java/ru/geekbrains/spring1/JavaCodeCreator.java
@@ -1,8 +1,18 @@
 package ru.geekbrains.spring1;
 
 public class JavaCodeCreator implements  CodeCreator{
+    public GenerateNames getGenerateNames() {
+        return generateNames;
+    }
+
+    public void setGenerateNames(GenerateNames generateNames) {
+        this.generateNames = generateNames;
+    }
+
+    private  GenerateNames generateNames;
+
     @Override
     public String javaCodeCreator() {
-        return "public class Cat {\n     \n}";
+        return "public class " +generateNames.createAnimalNames() + "{\n     \n}";
     }
 }
