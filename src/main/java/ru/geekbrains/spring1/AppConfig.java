9509793eadc9553baaea7f4e2cc54cefa241e54b
IMBABOT1 <zlovred12@gmail.com>
2021-02-16 02:53:44 +0300

    codeCreator

--- src/main/java/ru/geekbrains/spring1/AppConfig.java
+++ src/main/java/ru/geekbrains/spring1/AppConfig.java
@@ -1,4 +1,26 @@
 package ru.geekbrains.spring1;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
 public class AppConfig {
+
+
+ //<bean id="createAnimalNames" class="ru.geekbrains.spring1.AnimalNames">
+//</bean>
+    @Bean("createAnimalNames")
+   public AnimalNames createName(){
+        GenerateNames generateNames = new AnimalNames();
+        return (AnimalNames) generateNames;
+   }
+
+    @Bean("javaCodeCreator")
+    public CodeCreator codeCreator(){
+        CodeCreator codeCreator = new JavaCodeCreator();
+        ((JavaCodeCreator) codeCreator).setGenerateNames(createName());
+        return codeCreator;
+    }
 }
--- src/main/java/ru/geekbrains/spring1/MainApp.java
+++ src/main/java/ru/geekbrains/spring1/MainApp.java
@@ -1,31 +1,41 @@
 package ru.geekbrains.spring1;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MainApp {
 
+//    public static void main(String[] args) {
+//       // temp();
+//        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("Context.xml");
+//        AnnotatedSimpleBean annotatedSimpleBean = context.getBean("annotatedSimpleBean", AnnotatedSimpleBean.class);
+//        annotatedSimpleBean.setData("a");
+//        System.out.println(annotatedSimpleBean.getData());
+//
+//        annotatedSimpleBean.printRandomJavaClass();
+//
+//
+//    }
+//
+//    private static void temp() {
+//        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("Context.xml");
+//
+//        CodeCreator codeCreator = context.getBean("javaCodeCreator", CodeCreator.class);
+//        GenerateNames generateNames = context.getBean("createAnimalNames", AnimalNames.class);
+//        SimpleBean simpleBean = context.getBean("setField", SimpleBean.class);
+//        simpleBean.setField("s");
+//        System.out.println(simpleBean.getField());
+//        SimpleBean simpleBean1 = context.getBean("setField", SimpleBean.class);
+//        simpleBean1.setField("a");
+//        System.out.println(simpleBean1.getField());
+//    }
+
     public static void main(String[] args) {
-       // temp();
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("Context.xml");
-        AnnotatedSimpleBean annotatedSimpleBean = context.getBean("annotatedSimpleBean", AnnotatedSimpleBean.class);
-        annotatedSimpleBean.setData("a");
-        System.out.println(annotatedSimpleBean.getData());
+        ApplicationContext  context = new AnnotationConfigApplicationContext(AppConfig.class);
+        CodeCreator javacc = context.getBean("javaCodeCreator", CodeCreator.class);
+        System.out.println(javacc.javaCodeCreator());
+        ((AnnotationConfigApplicationContext) context).close();
 
-        annotatedSimpleBean.printRandomJavaClass();
-
-
-    }
-
-    private static void temp() {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("Context.xml");
-
-        CodeCreator codeCreator = context.getBean("javaCodeCreator", CodeCreator.class);
-        GenerateNames generateNames = context.getBean("createAnimalNames", AnimalNames.class);
-        SimpleBean simpleBean = context.getBean("setField", SimpleBean.class);
-        simpleBean.setField("s");
-        System.out.println(simpleBean.getField());
-        SimpleBean simpleBean1 = context.getBean("setField", SimpleBean.class);
-        simpleBean1.setField("a");
-        System.out.println(simpleBean1.getField());
     }
 }
